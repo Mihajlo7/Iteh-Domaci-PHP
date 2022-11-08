29ba994e70b73d855b60674b761bbc94a4c4caa4
Mihajlo Pavlovic <116083176+Mihajlo7@users.noreply.github.com>
2022-11-08 17:07:44 +0100

    Stranica za prijavu

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteka</title>
+</head>
+<body>
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    <form action="">
+        <h3>Prijava </h3>
+        
+        
+        <label for="username">Korisnicko ime</label>
+        <input type="text" placeholder="Email or Phone" id="username">
+
+        <label for="password">Sifra</label>
+        <input type="password" placeholder="Password" id="password">
+
+        <button>prijava</button>
+    </form>
+</body>
+</html>
